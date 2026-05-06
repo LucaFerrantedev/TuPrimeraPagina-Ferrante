e94ab956afdd5ea318690fe99099f83b87a42914
LucaFerrantedev <lucafer19@gmail.com>
2026-05-06 19:43:28 -0300

    ajustes a html

--- Catalogo/urls.py
+++ Catalogo/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.urls import path
 from productos.views import CategoriaCreateView, ComponenteCreateView, ComponentesListView, ComponenteDetailView, ComponenteUpdateView, MarcaCreateView, home
+from accounts.views import LoginView
 from django.contrib.auth.views import PasswordChangeView
 
 urlpatterns = [
@@ -11,7 +12,11 @@ urlpatterns = [
    path("productos/crear_categoria/", CategoriaCreateView.as_view(), name="categoria_create"),
    path("productos/crear_componente/", ComponenteCreateView.as_view(), name="componente_create"),
    path("productos/producto_detail", ComponenteDetailView.as_view(), name=""),
-   path("productos/componente_update", ComponenteUpdateView.as_view(), name="")
+   path("productos/componente_update", ComponenteUpdateView.as_view(), name=""),
+
+   path('login/', LoginView.as_view(template_name='accounts/login.html'), name='login'),
+   path('logout/', LogoutView.as_view(), name='logout'),
+
 #    path("cambiar_password/', PasswordChangeView.as_view(
 #         template_name='accounts/cambiar_password.html',
 #         success_url='/
